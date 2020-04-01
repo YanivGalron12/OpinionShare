662524b9047c736ce000b3652df746c2030166d7
YanivGalron12 <63013969+YanivGalron12@users.noreply.github.com>
2020-04-01 21:15:01 +0300

    first push

---
+++ app/src/main/java/com/example/androidfirabaseauth2019/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.androidfirabaseauth2019;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.widget.ProfilePictureView;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int MY_REQUEST_CODE = 3423 ;
+    List<AuthUI.IdpConfig> providers;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Init provider
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(), // Google Builder
+                new AuthUI.IdpConfig.FacebookBuilder().build() //Facebook Builder
+        );
+        showSignInOptions();
+    }
+
+
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.opinionshare_logo)
+                        .setTheme(R.style.AppTheme)
+                        .build(),
+                MY_REQUEST_CODE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MY_REQUEST_CODE){
+
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK){
+                //Get User
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+                Intent goToLogin = new Intent(MainActivity.this, ProfileActivity.class);
+
+                startActivity(goToLogin);
+                //Shoe Email on Toast
+                Toast.makeText(this,""+user.getEmail(),Toast.LENGTH_SHORT).show();
+
+            }
+            else {
+                Toast.makeText(this,""+response.getError().getMessage(),Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/androidfirabaseauth2019/Member.java
@@ -0,0 +1,81 @@
+package com.example.androidfirabaseauth2019;
+
+import java.util.List;
+
+public class Member {
+    private String Name, email, userId, userType;
+    private Integer Age, likeCounter;
+    private List<String> friendList, categoryList;
+
+    public Member(String name, String email, String userId) {
+        Name = name;
+        this.email = email;
+        this.userId = userId;
+    }
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public Integer getAge() {
+        return Age;
+    }
+
+    public void setAge(Integer age) {
+        Age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getuserId() {
+        return userId;
+    }
+
+    public void setuserId(String userId) {
+        this.userId = userId;
+    }
+
+    public Integer getLikeCounter() {
+        return likeCounter;
+    }
+
+    public void setLikeCounter(Integer likeCounter) {
+        this.likeCounter = likeCounter;
+    }
+
+    public List<String> getFriendList() {
+        return friendList;
+    }
+
+    public void setFriendList(List<String> friendList) {
+        this.friendList = friendList;
+    }
+
+    public List<String> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<String> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+
+
+}
