0b7bfef21060d3d31e000f289a7c2bc45f0929f1
YanivGalron12 <63013969+YanivGalron12@users.noreply.github.com>
2020-04-05 23:49:33 +0300

    OpinionShare: ver 05/04/20 23:47

---
+++ app/src/main/java/com/example/opinionshare/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.opinionshare;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+import com.facebook.login.widget.ProfilePictureView;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ErrorCodes;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.FirebaseUserMetadata;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Choose an arbitrary request code value
+    private static final int MY_REQUEST_CODE = 3423;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
+    FirebaseUserMetadata metadata;
+    List<AuthUI.IdpConfig> providers;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseUser user = auth.getCurrentUser();
+        if (user != null) {
+            // user is already signed in
+            startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            finish();
+            return; // need return because we don't need to execute showSignInOption and set providers
+        }
+
+        providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(), // Google Builder
+                new AuthUI.IdpConfig.FacebookBuilder().build() //Facebook Builder
+                // TODO: fix Facebook provider authentication - follow the guide at
+                // TODO: https://developers.facebook.com/apps/202893697669946/fb-login/quickstart/ At 2.6
+                // TODO: add email/phone authentication
+        );
+
+        showSignInOptions();
+
+    }
+
+    private void showSignInOptions() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.opinionshare_logo)
+                        .setTheme(R.style.AppTheme)
+                        .setIsSmartLockEnabled(false)
+                        .build(),
+                MY_REQUEST_CODE);
+        // we can add PrivacyPolicy by adding .setTosAndPrivacyPolicyUrls
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // MY_REQUEST_CODE is the request code you passed into startActivityForResult(...) when starting the sign in flow
+        if (requestCode == MY_REQUEST_CODE) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            // Successfully signed in
+            if (resultCode == RESULT_OK) {
+                //Get User  ONLY HERE the user is finished logging in
+                FirebaseUser user = auth.getCurrentUser();
+                // TODO: check what to do with comment on .getMetadata
+                metadata = user.getMetadata();
+                if (metadata.getCreationTimestamp() == metadata.getLastSignInTimestamp()) {
+                    // The user is new, show them a fancy intro screen!
+                    // TODO: pass to the next intent that this is a new user or check there if the user is new
+                    Toast.makeText(this,"Welcome to OpinionShare !!!!",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, RegisterActivity.class));
+                    finish();
+                } else {
+                    // This is an existing user.
+                    // TODO: show welcome back screen?
+                    Toast.makeText(this,"Welcome back my old friend",Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+                    finish();
+                }
+            } else {
+                // Sign in failed
+                Toast.makeText(this, "Failed to sign in", Toast.LENGTH_LONG).show();
+                // TODO: check what is it Snackbar - (Flutter?)
+                if (response == null) {
+                    // User pressed back button
+                    //showSnackbar(R.string.sign_in_cancelled);
+                    return;
+                }
+                if (response.getError().getErrorCode() == ErrorCodes.NO_NETWORK) {
+                    //showSnackbar(R.string.no_internet_connection);
+                    return;
+                }
+
+                //showSnackbar(R.string.unknown_error);
+                //Log.e(TAG, "Sign-in error: ", response.getError());
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/opinionshare/Member.java
@@ -0,0 +1,118 @@
+package com.example.opinionshare;
+
+import android.net.Uri;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+public class Member {
+    private String name;
+    private String email;
+    private String userId;
+    private String userType;
+    private String phoneNumber;
+    private String profilePhotoUri;
+    private Integer age, likeCounter;
+    private List<String> friendList, categoryList;
+
+
+    public Member(String userId, String name, String email, String profilePhotoUri, String phoneNumber) {
+        this.name = name;
+        this.email = email;
+        this.userId = userId;
+        this.userType = "Basic";
+        this.phoneNumber = phoneNumber;
+        this.profilePhotoUri = profilePhotoUri;
+        this.age = 1000;
+        this.likeCounter = 0;
+        this.friendList = new ArrayList<String>();
+        this.categoryList = new ArrayList<String>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public String getProfilePhotoUri() {
+        return profilePhotoUri;
+    }
+
+    public void setProfilePhotoUri(String profilePhotoUri) {
+        this.profilePhotoUri = profilePhotoUri;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getLikeCounter() {
+        return likeCounter;
+    }
+
+    public void setLikeCounter(Integer likeCounter) {
+        this.likeCounter = likeCounter;
+    }
+
+    public List<String> getFriendList() {
+        return friendList;
+    }
+
+    public void setFriendList(List<String> friendList) {
+        this.friendList = friendList;
+    }
+
+    public List<String> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<String> categoryList) {
+        this.categoryList = categoryList;
+    }
+}
